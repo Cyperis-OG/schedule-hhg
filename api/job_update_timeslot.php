45d05a9a749df9c5451f3102deb88f0dbee9ee22
Freeman <you@example.com>
2025-08-21 21:59:26 -0400

    Auto-commit: 2025-08-21T21:59:26-04:00

--- api/job_update_timeslot.php
+++ api/job_update_timeslot.php
@@ -17,7 +17,7 @@ if ($uid === '' || !preg_match('/^\d{4}-\d{2}-\d{2} \d\d:\d\d:\d\d$/', $start) |
 [$dateE, $timeE] = explode(' ', $end, 2);
 
 $stmt = $mysqli->prepare("UPDATE job_days SET work_date=?, start_time=?, end_time=?, contractor_id=COALESCE(?, contractor_id) WHERE uid=? LIMIT 1");
-$stmt->bind_param('sssds', $dateS, $timeS, $timeE, $contractor_id, $uid);
+$stmt->bind_param('sssis', $dateS, $timeS, $timeE, $contractor_id, $uid);
 $stmt->execute();
 
 echo json_encode(['ok' => true]);
