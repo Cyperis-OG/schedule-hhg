f908c609ebe693f0dd6df461c6f88bc81e114850
Freeman <you@example.com>
2025-08-24 23:55:00 -0400

    Auto-commit: 2025-08-24T23:55:00-04:00

--- assets/js/quickadd.js
+++ assets/js/quickadd.js
@@ -180,9 +180,9 @@
 
           <div class="files-row">
             <div class="qa-row">
-              <label>BOL / CSO (PDF, multiple)</label>
-              <input name="day.${index}.bol_files" type="file" accept="application/pdf" multiple />
-              <div class="file-hint">Attach one or more PDFs (bill of lading, CSO, etc.).</div>
+              <label>BOL / CSO (PDF)</label>
+              <input name="day.${index}.bol_files" type="file" accept="application/pdf" />
+              <div class="file-hint">Attach a PDF (bill of lading, CSO, etc.). Uploading a new file replaces the existing one.</div>
             </div>
             <div class="qa-row">
               <label>Additional files (any)</label>
