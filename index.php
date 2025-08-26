07eec7fce997745bf13ab5d7252c1eb3742dd57b
Freeman <you@example.com>
2025-08-26 00:18:37 -0400

    Auto-commit: 2025-08-26T00:18:37-04:00

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ include '/home/freeman/job_scheduler.php';
 
   <!-- App styles -->
   <link rel="stylesheet" href="./assets/app.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 </head>
 <body>
   <header class="topbar">
