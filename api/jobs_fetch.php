fc23415cd2bd49d723f29ccdeb870937296acaa3
Freeman <you@example.com>
2025-08-26 18:06:59 -0400

    Auto-commit: 2025-08-26T18:06:59-04:00

--- api/jobs_fetch.php
+++ api/jobs_fetch.php
@@ -70,7 +70,6 @@ $sqlE = "SELECT
            jd.pctechs,
            jd.supervisors,
            jd.project_managers,
-           jd.crew_transport,
            jd.electricians,
            jd.day_notes
          FROM job_days jd
@@ -112,7 +111,6 @@ if ($stmt = $mysqli->prepare($sqlE)) {
       'pctechs'        => (int)$row['pctechs'],
       'supervisors'    => (int)$row['supervisors'],
       'project_managers'=> (int)$row['project_managers'],
-      'crew_transport'  => (int)$row['crew_transport'],
       'electricians'    => (int)$row['electricians'],
       'day_notes'       => $row['day_notes']
     ];
