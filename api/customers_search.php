db9f26d7fb84267ecc249dae0b922cc8401ae168
Freeman <you@example.com>
2025-08-31 00:02:03 -0400

    Auto-commit: 2025-08-31T00:02:03-04:00

--- api/customers_search.php
+++ api/customers_search.php
@@ -7,8 +7,9 @@
  *  - default_salesman
  *  - preferred_contractor_id (+ its name)
  *  - last_job_number
+ *  - standard_notes
  *
- * Output: { results: [ { id, name, default_location, default_salesman, preferred_contractor_id, preferred_contractor_name, last_job_number } ... ] }
+ * Output: { results: [ { id, name, default_location, default_salesman, preferred_contractor_id, preferred_contractor_name, last_job_number, standard_notes } ... ] }
  */
 include '/home/freeman/job_scheduler.php';
 header('Content-Type: application/json; charset=utf-8');
@@ -22,6 +23,7 @@ if ($q === '' || mb_strlen($q) < 1) {
 $like = '%' . $mysqli->real_escape_string($q) . '%';
 
 $sql = "SELECT c.id, c.name, c.default_location, c.default_salesman, c.preferred_contractor_id, c.last_job_number,
+               c.standard_notes,
                ct.name AS preferred_contractor_name
         FROM customers c
         LEFT JOIN contractors ct ON ct.id = c.preferred_contractor_id
@@ -39,7 +41,8 @@ while ($r = $res->fetch_assoc()) {
     'default_salesman' => $r['default_salesman'],
     'preferred_contractor_id' => $r['preferred_contractor_id'] ? (int)$r['preferred_contractor_id'] : null,
     'preferred_contractor_name' => $r['preferred_contractor_name'],
-    'last_job_number' => $r['last_job_number']
+    'last_job_number' => $r['last_job_number'],
+    'standard_notes' => $r['standard_notes']
   ];
 }
 echo json_encode(['results' => $out], JSON_UNESCAPED_UNICODE);
