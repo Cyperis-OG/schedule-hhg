a68dd804f6de3d217fbd5404b3fb2cff68d86dd5
Cyperis-OG <clint.freeman@parisarms.com>
2025-09-03 17:31:54 -0500

    auto: 2025-09-03 17:31:54 CDT [armstrong]

--- view_contractor_schedule.php
+++ view_contractor_schedule.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 session_start();
-require_once '/home/freeman/job_scheduler.php';
+require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/lib/magic_link.php';
 
 $cid  = $_GET['contractor_id'] ?? '';
